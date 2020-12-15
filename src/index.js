c4e96c3875c8f564772fa2a28b9dc31ce13664b4
blumenthaler <blumenthaleralex@gmail.com>
2020-12-15 16:28:46 -0500

    basic redux store setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { thunk } from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const users = () => []
+
+const reducer = combineReducers({
+  users
+})
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
