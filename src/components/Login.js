105fde5faa32dcf98542bb60b6cd5fb9954e5c19
blumenthaler <blumenthaleralex@gmail.com>
2020-12-16 10:57:42 -0500

    controlled input for username/password

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,15 +7,19 @@ class Login extends React.Component {
         password: ""
     }
 
-    handleUsername = event => {
-        console.log(event.target.value)
+    handleChange = event => {
+        this.setState({
+            ...state,
+            [event.target.name]: event.target.value
+        })
     }
 
     render() {
         return (
             <form>
-                <input type="text" name="username" onChange={this.handleUsername}></input>
-                <input type="password" name="password"></input>
+                <input type="text" name="username" onChange={this.handleChange} value={this.state.username}></input>
+                <input type="password" name="password" onChange={this.handleChange} value={this.state.password}></input>
+                <input type="submit" value="Login"></input>
             </form>
         )
     }
