e7fae9db665386812c8207d9ff158c68cf5cb0c9
blumenthaler <blumenthaleralex@gmail.com>
2020-12-16 10:54:05 -0500

    created updated login action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Login from './components/Login.js'
 
 class App extends React.Component {
 
   render () {
   return (
-   <div>Howdy Globe</div>
+   <Login />
   );
   }
 }
--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,3 +1,4 @@
+// sync
 export const setCurrentUser = user => {
     return {
         type: 'SET_CURRENT_USER',
@@ -9,11 +10,16 @@ export const setCurrentUser = user => {
 
 
 
-
+// async
 export const login = credentials => {
+    // credentials => username: "Mike Q", password: "pw"
     return dispatch => {
         return fetch('http://localhost:3000/api/v1/login',{
-            method: "POST"
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({username: "Mike Q", password: "pw"})
         })
     }
 }
