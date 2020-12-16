456fa8772c589c5a3d6b3f8580cf420409c6f58a
blumenthaler <blumenthaleralex@gmail.com>
2020-12-16 11:04:24 -0500

    fixed state syntax in handleChange

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,14 +9,18 @@ class Login extends React.Component {
 
     handleChange = event => {
         this.setState({
-            ...state,
             [event.target.name]: event.target.value
         })
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+        // add prop to login (redux)
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input type="text" name="username" onChange={this.handleChange} value={this.state.username}></input>
                 <input type="password" name="password" onChange={this.handleChange} value={this.state.password}></input>
                 <input type="submit" value="Login"></input>
