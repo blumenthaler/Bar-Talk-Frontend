2d766a6e6bb2eda2b909b1f42f702766842db224
blumenthaler <blumenthaleralex@gmail.com>
2020-12-16 11:29:37 -0500

    mapped state, dispatch to props, passed login dispatch to login cform

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Login from '../components/Login.js';
 
 class LoginContainer extends React.Component {
 
     render() {
+        // console.log(this.props.state)
+        // console.log(this.props.loginUser)
         return (
-            <Login />
+            <Login loginUser={this.props.loginUser}/>
         )
     }
 }
 
-// map state/dispatch to props here
+const mapStateToProps = state => {
+    return {
+        state
+    }
+}
 
-export default LoginContainer
+// map state/dispatch to props here
+const mapDispatchToProps = dispatch => {
+    return {
+        loginUser: user => dispatch({type: "LOGIN_USER", user})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer)
