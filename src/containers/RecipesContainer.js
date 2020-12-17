edc416b3668ae471a54aa9c2575cde4755e6b94a
blumenthaler <blumenthaleralex@gmail.com>
2020-12-17 13:46:03 -0500

    mapped dispatch to props (getAllRecipes)

--- src/containers/RecipesContainer.js
+++ src/containers/RecipesContainer.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Recipes from '../components/Recipes.js';
 // import { loginUser } from '../actions/loginUser.js';
+import { getAllRecipes } from '../actions/recipes.js';
 
 class RecipesContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.getAllRecipes()
+    }
 
     render() {
         // console.log(this.props.state)
@@ -28,4 +33,4 @@ const mapStateToProps = state => {
 // }
 
 // export default connect(mapStateToProps, mapDispatchToProps)(RecipesContainer)
-export default connect(mapStateToProps)(RecipesContainer)
+export default connect(mapStateToProps, {getAllRecipes})(RecipesContainer)
