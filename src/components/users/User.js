f7f7cdda25172b3ae113452024cb5c732e707ec0
blumenthaler <blumenthaleralex@gmail.com>
2020-12-18 15:01:12 -0500

    personalized user name display if it is their recipe

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,11 +3,18 @@ import RecipesContainer from '../containers/RecipesContainer.js';
 
 class User extends React.Component {
     render() {
-        
+        let title;
+        if (this.props.currentUser.id === this.props.user.id) {
+            title = "Your Recipe:"
+        }
+        else {
+            title = `${this.props.user.username}'s Recipe:`
+        }
+
         return (
             <>
-            <h4>{this.props.user.username}'s Recipe:</h4> 
-            <RecipesContainer recipes={this.props.recipes} user={this.props.user} currentUser={this.props.currentUser} />
+                <h4>{title}</h4> 
+                <RecipesContainer recipes={this.props.recipes} user={this.props.user} currentUser={this.props.currentUser} />
             </>
         )
     }
