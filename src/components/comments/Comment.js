0d7e0f31e16f843320555e6cb1ab6f59767aaf1d
blumenthaler <blumenthaleralex@gmail.com>
2020-12-18 15:34:24 -0500

    created mock data for comment component

---
+++ src/components/comments/Comment.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class Comment extends React.Component {
+    render() {
+
+        return (
+            <li>
+                this do be a comment tho
+            </li>
+        )
+    }
+}
+
+export default Comment
