d38bdc031b012488847eab6b766d2964f81c0b2c
blumenthaler <blumenthaleralex@gmail.com>
2020-12-18 16:06:25 -0500

    filtered each recipe's comments, passed as props

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -4,6 +4,7 @@ import Comment from './Comment.js'
 class Comments extends React.Component {
 
     render() {
+        console.log(this.props)
             return (
                 
                 <ul>
