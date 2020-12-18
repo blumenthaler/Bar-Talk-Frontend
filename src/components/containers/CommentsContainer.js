6a05c059654959260d3061e3118b4e7706267a46
blumenthaler <blumenthaleralex@gmail.com>
2020-12-18 15:20:58 -0500

    created basic comments container, components

---
+++ src/components/containers/CommentsContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import Cocktails from '../cocktails/Cocktails.js';
+// import { getAllCocktails } from '../../actions/cocktails.js';
+
+class CommentsContainer extends React.Component {
+
+    // componentDidMount() {
+    //     this.props.getAllCocktails()
+    // }
+
+    render() {
+
+            return (
+                <div>
+                    This is the comments container
+                </div>
+            )
+    }
+}
+
+// const mapStateToProps = state => {
+//     return {
+//         cocktails: state.cocktails,
+//     }
+// }
+
+// export default connect(mapStateToProps, {getAllCocktails})(CommentsContainer)
+export default CommentsContainer
