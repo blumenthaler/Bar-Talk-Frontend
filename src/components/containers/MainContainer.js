edb5d3aa0f74e66bdd9eabb50145d683d34dd685
blumenthaler <blumenthaleralex@gmail.com>
2020-12-21 15:12:16 -0500

    created main container

---
+++ src/components/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MainContainer extends React.Component {
+
+    render() {
+        return (
+            <div>This is the Main Container</div>
+        )
+    }
+}
+
+export default MainContainer
