8d737d89c3bb2640c3c81c0f31304bb149d86b58
blumenthaler <blumenthaleralex@gmail.com>
2020-12-21 14:32:56 -0500

    updated comments props; included users

--- src/components/containers/CommentsContainer.js
+++ src/components/containers/CommentsContainer.js
@@ -10,17 +10,28 @@ class CommentsContainer extends React.Component {
     }
 
     render() {
+        if ((this.props.comments.loading) || (!this.props.comments.comments.data)) {
+            return (<h2>Loading...</h2>)
+        }
+        else {
+
+        // const {comments} = this.props
+        // console.log(this.props)
             return (
                 <div>
-                    <Comments comments={this.props.comments.comments.filter(comment => (this.props.recipe.relationships.comments.data.map(comment => comment.id)).includes(comment.id.toString()))} /> 
+                    {/* <Comments /> */}
+                    <Comments comments={this.props.comments.comments.data.filter(comment => (this.props.recipe.relationships.comments.data.map(comment => comment.id)).includes(comment.id.toString()))}
+                    users={this.props.users.users} /> 
                 </div>
             )
+            }    
     }
 }
 
 const mapStateToProps = state => {
     return {
         comments: state.comments,
+        users: state.users
     }
 }
 
