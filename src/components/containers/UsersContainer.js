6ec866a7d6513a9f2ed5c93c27989c2330f6654f
blumenthaler <blumenthaleralex@gmail.com>
2020-12-28 16:54:19 -0500

    skips Users component if rendering profile page

--- src/components/containers/UsersContainer.js
+++ src/components/containers/UsersContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getAllUsers } from '../../actions/users.js';
 import Users from '../users/Users.js';
+import User from '../users/User.js';
 
 
 class UsersContainer extends React.Component {
@@ -11,28 +12,39 @@ class UsersContainer extends React.Component {
             return (<h2>Loading...</h2>)
         }
         else {
-
-        // only pass recipes, users as props if they belong to this specific cocktail
-        const filteredRecipes = this.props.recipes.filter(recipe => recipe.relationships.cocktail.data.id === this.props.cocktail.id)
-
-        const filteredUsers = this.props.users.filter(user => (filteredRecipes.map(recipe => recipe.relationships.user.data.id)).includes(user.id))
-
-        // sorts users, current user listed first
-        const sorted = filteredUsers.reduce((acc, user) => {
-            if (user.username === this.props.currentUser.username) {
-                return [user, ...acc];
-            }
-            return [...acc, user]
-        }, [])
-        
-            return (
-                <Users 
-                    users={sorted} 
+            // only pass recipes, users as props if they belong to this specific cocktail
+            const filteredRecipes = this.props.recipes.filter(recipe => recipe.relationships.cocktail.data.id === this.props.cocktail.id)
+            
+            if (this.props.profile) {
+                return (
+                    <User 
+                    user={this.props.currentUser.data} 
                     cocktail={this.props.cocktail}
                     currentUser={this.props.currentUser}
                     recipes={filteredRecipes}
-                />
-            )
+                    />
+                )
+            }
+            else {
+
+            const filteredUsers = this.props.users.filter(user => (filteredRecipes.map(recipe => recipe.relationships.user.data.id)).includes(user.id))
+
+            // sorts users, current user listed first
+            const sorted = filteredUsers.reduce((acc, user) => {
+                if (user.username === this.props.currentUser.username) {
+                    return [user, ...acc];
+                }
+                    return [...acc, user]
+                }, [])
+                return (
+                    <Users 
+                        users={sorted} 
+                        cocktail={this.props.cocktail}
+                        currentUser={this.props.currentUser}
+                        recipes={filteredRecipes}
+                    />
+                )
+            }
         }
     }
 }
