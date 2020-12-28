d1617cf36cdb46d66c8ac094c1ddda0449868763
blumenthaler <blumenthaleralex@gmail.com>
2020-12-28 15:31:28 -0500

    changed Users component to presentational function (not class), removed unused code

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,27 +1,19 @@
 import React from 'react'
 import User from './User.js'
 
-class Users extends React.Component {
-
-    render() {
-        if (this.props.users.length === 0) {
+const Users = props => {
+        if (props.users.length === 0) {
             return (
                 <h2>loading...</h2>
             )
         }
         else {
-            console.log(this.props)
-            console.log(this.props.users[0].id === this.props.currentUser.data.id)
             return (
                 <ol>
-                    {this.props.users.map(user => <User key={user.id} currentUser={this.props.currentUser} user={user} />)}
-                  
+                    {props.users.map(user => <User key={user.id} currentUser={props.currentUser} user={user} recipes={props.recipes} />)}
                 </ol>
             )
         }
-    }
 }
 
 export default Users
-
-// recipes={this.props.recipes.filter(recipe => recipe.relationships.user.data.id === user.id.toString() )} />)}
