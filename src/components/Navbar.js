6d827a49a804ad13ee4e0215e9c774aaaf7027b7
blumenthaler <blumenthaleralex@gmail.com>
2020-12-28 13:13:51 -0500

    began updating navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {NavLink} from 'react-router-dom';
 
-class Navbar extends React.Component {
-    render() {
-        return (
-            <div>This is a navbar</div>
-        )
+const NavBar = ({ currentUser, loggedIn }) => {
+    return (
+        <div className="navbar">
+          {
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>              
+              </li>
+              <li>
+                <NavLink to="/cocktails">All Cocktails</NavLink>
+              </li>
+              <li>
+                <NavLink to="/popular">Popular Recipes</NavLink>
+              </li>
+            </ul>
+          </nav>
+          }
+        </div>
+      );
+}
+
+const mapStateToProps = ({currentUser}) => {
+    return {
+      currentUser,
+      loggedIn: !!currentUser
     }
 }
 
-export default Navbar
+export default connect(mapStateToProps)(NavBar)
