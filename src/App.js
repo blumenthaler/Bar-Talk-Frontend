a316c8d02933202fbc33854147c98e35014f6199
blumenthaler <blumenthaleralex@gmail.com>
2020-12-28 13:25:43 -0500

    began implementing navlinks in App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import React from 'react';
 import Login from './components/users/Login.js'
 import Signup from './components/users/Signup.js'
 import Logout from './components/users/Logout.js'
+import NavBar from './components/NavBar.js'
 // import RecipesContainer from './containers/RecipesContainer.js'
 import CocktailsContainer from './components/containers/CocktailsContainer.js'
 import { getCurrentUser } from './actions/currentUser.js';
 import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 
 class App extends React.Component {
 
@@ -16,9 +23,26 @@ class App extends React.Component {
   }
 
   render () {
-  return (
-    this.props.currentUser ? <><Logout /><CocktailsContainer currentUser={this.props.currentUser}/></> : <><Login /> <Signup /> </>
-  );
+    return (
+      <Router>
+        {
+          <div>
+            <NavBar />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              {/* <Route exact path="/cocktails">
+                <CocktailsContainer />
+              </Route>
+              <Route exact path="/popular">
+                <PopularRecipesContainer />
+              </Route> */}
+            </Switch>
+          </div>
+        }
+      </Router>
+    );
   }
 }
 
@@ -29,3 +53,5 @@ const mapStateToProps = ({ currentUser }) => {
 } 
 
 export default connect(mapStateToProps, { getCurrentUser })(App);
+
+// this.props.currentUser ? <><Logout /><CocktailsContainer currentUser={this.props.currentUser}/></> : <><Login /> <Signup /> </>
