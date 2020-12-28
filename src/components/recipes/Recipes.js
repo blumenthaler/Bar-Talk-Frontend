4ec7744ce0ff1ee16211d962437bc0c0b24ad664
blumenthaler <blumenthaleralex@gmail.com>
2020-12-28 15:21:34 -0500

    changed Recipes component to presentational function (not class)

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import Recipe from './Recipe.js'
 
-class Recipes extends React.Component {
-
-    render() {
-        if (!this.props.recipes) {
-            return (
-                <h2>loading...</h2>
-            )
-        }
-        else {
-            return (
-                
-                <ol>
-                    {this.props.recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} userId={recipe.relationships.user.data.id.toString()}  currentUser={this.props.currentUser} />)}
-                </ol>
-            )
-        }
+const Recipes = props => {
+    if (!props.recipes) {
+        return (
+            <h2>loading...</h2>
+        )
+    }
+    else {
+        return (            
+            <ol>
+                {props.recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} userId={props.user.id}  currentUser={props.currentUser} user={props.user}/>)}
+            </ol>
+        )
     }
 }
 
