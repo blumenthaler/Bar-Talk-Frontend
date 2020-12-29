9d254bb6ef7d7f427fa2965bf8cad1b1697c6f9e
blumenthaler <blumenthaleralex@gmail.com>
2020-12-29 15:24:11 -0500

    created successful fetch for posting new recipe

--- src/actions/recipes.js
+++ src/actions/recipes.js
@@ -33,3 +33,51 @@ export const getAllRecipes = () => {
       .catch(console.log)
     }
 }
+
+// Add new Recipe to the database
+export const addRecipe = (recipe, user, cocktail) => {
+  console.log(recipe)
+  console.log(user)
+  console.log(cocktail)
+  const {name, spirit, ingredients, garnish, notes} = recipe
+  const user_id = user.id
+  const cocktail_id = cocktail.id
+
+  const data = {
+    recipe: {
+      name: name,
+      spirit: spirit,
+      ingredients: ingredients,
+      garnish: garnish,
+      notes: notes,
+      votes: 0,
+      user_id: user_id,
+      cocktail_id: cocktail_id
+    }
+  }
+  console.log(data)
+
+  return dispatch => {
+    dispatch({type: "ADDING_NEW_RECIPE"})
+    return fetch(`${DOMAIN_URL}/api/v1/recipes`, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      .then(resp => resp.json())
+      .then(recipes => {
+          if (recipes.error) {
+            console.log(recipes.error)
+          }
+          else {
+            console.log('success')
+            // show the user's new recipe!
+            // dispatch(getRecipes(recipes))
+          }
+      })
+      .catch(console.log)
+  }
+}
