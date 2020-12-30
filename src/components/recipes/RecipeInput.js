e378100ef2be45b3d2ae86d81c93769c7fb306c3
blumenthaler <blumenthaleralex@gmail.com>
2020-12-30 15:44:50 -0500

    added loading logic for multiple use cases (with or without cocktail)

--- src/components/recipes/RecipeInput.js
+++ src/components/recipes/RecipeInput.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addRecipe} from '../../actions/recipes.js'
 
 class RecipeInput extends React.Component {
     
@@ -13,15 +15,36 @@ class RecipeInput extends React.Component {
 
     handleOnSubmit = event => {
         event.preventDefault()
-        this.props.addRecipe(this.state, this.props.currentUser.data, this.props.cocktail)  
-        this.setState({
-            name: "",
-            spirit: "",
-            ingredients: "",
-            garnish: "",
-            notes: "",
-            votes: 0
-        })    
+        if (!!this.props.cocktail) {
+            let name = document.getElementById(`${this.props.cocktail.id}_name`).innerText
+            let spirit = document.getElementById(`${this.props.cocktail.id}_spirit`).innerText
+            let sendable = {
+                ...this.state,
+                name,
+                spirit
+            }
+            this.props.addRecipe(sendable, this.props.currentUser.data)
+            this.setState({
+                name,
+                spirit,
+                ingredients: "",
+                garnish: "",
+                notes: "",
+                votes: 0
+            })
+            
+        }
+        else {
+            this.props.addRecipe(this.state, this.props.currentUser.data)  
+            this.setState({
+                name: "",
+                spirit: "",
+                ingredients: "",
+                garnish: "",
+                notes: "",
+                votes: 0
+            })   
+        } 
     }
 
     handleOnChange = event => {
@@ -31,11 +54,14 @@ class RecipeInput extends React.Component {
     }
 
     render() {
+        
+        
         return (
             <>
             <form onSubmit={(event) => this.handleOnSubmit(event)}> 
-                <input type="text" name="name" onChange={this.handleOnChange} value={this.state.name}/><br />
-                <input type="text" name="spirit" onChange={this.handleOnChange} value={this.state.spirit}/><br />
+                {!this.props.cocktail ? <><input type="text" name="name" onChange={this.handleOnChange} value={this.state.name}/><br />
+                <input type="text" name="spirit" onChange={this.handleOnChange} value={this.state.spirit}/><br /></> : <><div id={this.props.cocktail.id + '_name'} >{this.props.cocktail.attributes.name}</div><div id={this.props.cocktail.id + '_spirit'}>{this.props.cocktail.attributes.spirit.charAt(0).toUpperCase() + this.props.cocktail.attributes.spirit.slice(1)}</div></>}
+                
                 <input type="text" name="ingredients" onChange={this.handleOnChange} value={this.state.ingredients}/><br />
                 <input type="text" name="garnish" onChange={this.handleOnChange} value={this.state.garnish}/><br />
                 <input type="text" name="notes" onChange={this.handleOnChange} value={this.state.notes}/><br />
@@ -47,4 +73,4 @@ class RecipeInput extends React.Component {
     }
 }
 
-export default RecipeInput
+export default connect(null, {addRecipe})(RecipeInput)
