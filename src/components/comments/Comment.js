c9d987dcb33c40cae2183c35e3964b468e67a130
blumenthaler <blumenthaleralex@gmail.com>
2021-01-04 17:53:10 -0500

    render delete comment button if belongs to currentUser

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+export default class Comment extends React.Component {
+    
+    handleDelete = () => {
+        this.props.deleteComment()
+    }
 
-const Comment = props => {
+    render() {
     return (
-        <li>
-            {props.user.attributes.username}: "{props.comment.attributes.content}"
-        </li>
-    )
+            <>
+                <li>
+                    {this.props.user.attributes.username}: "{this.props.comment.attributes.content}"
+                </li>
+                {this.props.user.id === this.props.currentUser.data.id ? <button onClick={() => this.handleDelete}  >Delete</button> : null}
+            </>
+        )
+    }
 }
-
-export default Comment
