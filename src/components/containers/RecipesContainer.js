5e5aaa040f687764a664ad8f44706bd13a33e453
blumenthaler <blumenthaleralex@gmail.com>
2021-01-04 13:47:56 -0500

    renders RecipeInput, NewRecipeButton as needed

--- src/components/containers/RecipesContainer.js
+++ src/components/containers/RecipesContainer.js
@@ -4,17 +4,38 @@ import { connect } from 'react-redux';
 import Recipes from '../recipes/Recipes.js';
 import RecipeInput from '../recipes/input/RecipeInput.js';
 import { addRecipe } from '../../actions/recipes.js';
+import { NewRecipeButton } from '../recipes/input/NewRecipeButton.js';
 
 
 class RecipesContainer extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            isEmptyState: true,
+            showingRecipeForm: false
+        }
+    }
+
+    triggerRecipeForm = () => {
+        this.setState({
+            showingRecipeForm: !this.state.showingRecipeForm,
+            isEmptyState: !this.state.isEmptyState
+        })
+    }
+
     render() {
+        console.log(this.props)
         const filteredRecipes = this.props.recipes.filter(recipe => recipe.relationships.user.data.id === this.props.user.id)
+
+
         
         return (
             <div>
                 <Recipes user={this.props.user} recipes={filteredRecipes} currentUser={this.props.currentUser} />
-                <RecipeInput currentUser={this.props.currentUser} addRecipe={this.props.addRecipe} cocktail={this.props.cocktail} history={this.props.history}/>
+
+                {this.state.showingRecipeForm ? <RecipeInput currentUser={this.props.currentUser} addRecipe={this.props.addRecipe} cocktail={this.props.cocktail} history={this.props.history} triggerRecipeForm={this.triggerRecipeForm}/> : 
+                <NewRecipeButton currentUser={this.props.currentUser} addRecipe={this.props.addRecipe} cocktail={this.props.cocktail} history={this.props.history} triggerRecipeForm={this.triggerRecipeForm}/>}
             </div>
         )
     }
