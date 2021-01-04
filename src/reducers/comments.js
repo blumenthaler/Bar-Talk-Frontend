c61fdea8e58663585e07a0cdda9abb6dde2ee1ee
blumenthaler <blumenthaleralex@gmail.com>
2021-01-04 17:06:04 -0500

    successfully adds new comment and associated data

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -9,11 +9,36 @@ export default (state = {
                 loading: true
             }
         case "ADD_COMMENTS":
+            console.log(state)
             return {
                 ...state,
                 comments: action.comments,
                 loading: false
             }
+        case "ADDING_NEW_COMMENT":
+            return {
+                ...state,
+                loading: true
+            }
+        case "ADD_NEW_COMMENT":
+            
+            const thisUser = action.comment.included.find(data => data.type === 'user')
+            const thisRecipe = action.comment.included.find(data => data.type === 'recipe')
+            
+            if (!state.comments.included.filter(data => data.type === "user").find(user => user.id === thisUser.id)) {
+                state.comments.included.push(thisUser)
+            }
+
+            if (!state.comments.included.find(recipe => recipe.id === thisRecipe.id)) {
+                state.comments.included.push(thisRecipe)
+            }
+            
+            state.comments.data.push(action.comment.data)
+            return {
+                ...state,
+                loading: false
+            }
+          
         default:
             return state;
     }
