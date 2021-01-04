1a6d6927258d5f7efb090cba03fc216caa37e8df
blumenthaler <blumenthaleralex@gmail.com>
2021-01-04 14:19:18 -0500

    toggles between new comment button and comment form

--- src/components/containers/CommentsContainer.js
+++ src/components/containers/CommentsContainer.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Comments from '../comments/Comments.js';
 import { getAllComments } from '../../actions/comments.js';
+import { NewCommentButton } from '../comments/NewCommentButton.js';
+import CommentInput from '../comments/CommentInput.js'
 
 class CommentsContainer extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isEmptyState: true,
+            showingCommentForm: false
+        }
+    }
+
+    triggerCommentForm = () => {
+        this.setState({
+            showingCommentForm: !this.state.showingCommentForm,
+            isEmptyState: !this.state.isEmptyState
+        })
+    }
 
     componentDidMount() {
         this.props.getAllComments()
@@ -18,10 +35,13 @@ class CommentsContainer extends React.Component {
         const comments = this.props.comments.comments.data.filter(comment => (this.props.recipe.relationships.comments.data.map(comment => comment.id)).includes(comment.id.toString()))
         
             return (
+                <>
                 <div>
                     <Comments comments={comments}
-                    users={users} /> 
+                    users={users} /><br />
+                    {!this.state.showingCommentForm ? <NewCommentButton triggerCommentForm={this.triggerCommentForm}/> : <CommentInput user={this.props.currentUser} recipe={this.props.recipe} triggerCommentForm={this.triggerCommentForm}/> }
                 </div>
+                </>
             )
             }    
     }
