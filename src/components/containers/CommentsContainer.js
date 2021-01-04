18f4e3b0418b85b325bc346ae1539cdde4d8c3c3
blumenthaler <blumenthaleralex@gmail.com>
2021-01-04 17:00:32 -0500

    git restore --staged src/components/containers/CommentsContainer.js

--- src/components/containers/CommentsContainer.js
+++ src/components/containers/CommentsContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Comments from '../comments/Comments.js';
-import { getAllComments } from '../../actions/comments.js';
+import { getAllComments, addComment } from '../../actions/comments.js';
 import { NewCommentButton } from '../comments/NewCommentButton.js';
 import CommentInput from '../comments/CommentInput.js'
+
 
 class CommentsContainer extends React.Component {
 
@@ -27,10 +28,14 @@ class CommentsContainer extends React.Component {
     }
 
     render() {
+       
         if ((this.props.comments.loading) || (!this.props.comments.comments.data)) {
             return (<h2>Loading...</h2>)
         }
         else {
+
+        console.log(this.props.comments)
+
         const users = this.props.included.filter(data => data.type === "user")
         const comments = this.props.comments.comments.data.filter(comment => (this.props.recipe.relationships.comments.data.map(comment => comment.id)).includes(comment.id.toString()))
         
@@ -39,19 +44,19 @@ class CommentsContainer extends React.Component {
                 <div>
                     <Comments comments={comments}
                     users={users} /><br />
-                    {!this.state.showingCommentForm ? <NewCommentButton triggerCommentForm={this.triggerCommentForm}/> : <CommentInput user={this.props.currentUser} recipe={this.props.recipe} triggerCommentForm={this.triggerCommentForm}/> }
+                    {!this.state.showingCommentForm ? <NewCommentButton triggerCommentForm={this.triggerCommentForm}/> : <CommentInput user={this.props.currentUser} recipe={this.props.recipe} triggerCommentForm={this.triggerCommentForm} addComment={this.props.addComment}/> }
                 </div>
                 </>
             )
-            }    
+        }    
     }
 }
 
 const mapStateToProps = state => {
     return {
         comments: state.comments,
-        included: state.comments.comments.included
+        included: state.comments.comments.included,
     }
 }
 
-export default connect(mapStateToProps, {getAllComments})(CommentsContainer)
+export default connect(mapStateToProps, {getAllComments, addComment})(CommentsContainer)
