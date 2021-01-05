69959ab5f67ed59e95cb2e52866b1992ddd19829
blumenthaler <blumenthaleralex@gmail.com>
2021-01-05 11:36:41 -0500

    removed unused code

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default class Comment extends React.Component {
     
-    handleDelete = () => {
-        this.props.deleteComment()
+    handleDelete = event => {
+        event.preventDefault()
+        this.props.deleteComment(this.props.comment)
     }
 
     render() {
@@ -12,7 +13,7 @@ export default class Comment extends React.Component {
                 <li>
                     {this.props.user.attributes.username}: "{this.props.comment.attributes.content}"
                 </li>
-                {this.props.user.id === this.props.currentUser.data.id ? <button onClick={() => this.handleDelete}  >Delete</button> : null}
+                {this.props.user.id === this.props.currentUser.data.id ? <button onClick={event => this.handleDelete(event)}  >Delete</button> : null}
             </>
         )
     }
