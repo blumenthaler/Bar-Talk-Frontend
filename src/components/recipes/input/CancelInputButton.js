78081a53c6d64ab09f2e83d7b473dc447c55c51a
blumenthaler <blumenthaleralex@gmail.com>
2021-01-05 18:08:52 -0500

    created CancelInputButton to implement useHistory

---
+++ src/components/recipes/input/CancelInputButton.js
@@ -0,0 +1,14 @@
+import { useHistory, useRouteMatch } from 'react-router-dom'
+
+export const CancelInputButton = props => {
+    const history = useHistory()
+    const match = useRouteMatch()
+    const handleClick = () => {
+        props.triggerRecipeForm()
+        history.push(`${match.url}`)
+        // console.log(match.url)
+    }
+    return (
+        <button onClick={() => handleClick()}>Cancel</button>
+    )
+}
