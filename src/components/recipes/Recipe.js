255e53af27bc4797ef3008492fa78ce9c36edb00
blumenthaler <blumenthaleralex@gmail.com>
2021-01-05 13:34:19 -0500

    changed to class component, state changes indicate whether to render edit form

--- src/components/recipes/Recipe.js
+++ src/components/recipes/Recipe.js
@@ -1,27 +1,57 @@
 import React from 'react'
 import CommentsContainer from '../containers/CommentsContainer.js'
+import { EditRecipeButton } from './edit/EditRecipeButton.js'
+import {EditRecipeForm} from './edit/EditRecipeForm'
 
-const Recipe = props => {
-        const currentUserId = props.currentUser.data.id
-        const recipeUserId = props.recipe.relationships.user.data.id
-        const {attributes} = props.recipe
-        return (
-            <li>
-                {attributes.name}
-                <br />
-                {attributes.ingredients}
-                <br />
-                {attributes.garnish}
-                <br />
-                {attributes.notes}
-                <br />
-                {attributes.votes}
-                <br />
-                {currentUserId === recipeUserId ? <><button>Edit Recipe</button><br /><button>Delete Recipe</button></> : null}
-                <CommentsContainer recipe={props.recipe} currentUser={props.currentUser} comments={props.comments}/>
-                <br /><br />
-            </li>
-        )
+class Recipe extends React.Component {
+        
+        constructor(props) {
+            super(props)
+            this.state = {
+                isEditing: false
+            }
+        }
+
+        triggerEditingForm = () => {
+            this.setState(prevState => {
+                return {
+                    isEditing: !prevState.isEditing
+                }
+            })
+        }
+        
+        render() {
+            const currentUserId = this.props.currentUser.data.id
+            const recipeUserId = this.props.recipe.relationships.user.data.id
+            const {attributes} = this.props.recipe
+            if (this.state.isEditing) {
+                return (
+                    <>
+                        <EditRecipeForm recipe={this.props.recipe} triggerEditingForm={this.triggerEditingForm}/>
+                    </>
+                )
+            }
+            else {
+                return (
+                    <li>
+                        {attributes.name}
+                        <br />
+                        {attributes.ingredients}
+                        <br />
+                        {attributes.garnish}
+                        <br />
+                        {attributes.notes}
+                        <br />
+                        {attributes.votes}
+                        <br />
+                        {currentUserId === recipeUserId ? <><EditRecipeButton triggerEditingForm={this.triggerEditingForm}/><br /><button>Delete Recipe</button></> : null}
+                        <CommentsContainer recipe={this.props.recipe} currentUser={this.props.currentUser} comments={this.props.comments}/>
+                        <br /><br />
+                    </li>
+                )
+            }
+        }
+
 
 }
 
