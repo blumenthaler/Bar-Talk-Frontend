8597893bce278335b44b4b9e6903d7e53fbb194c
blumenthaler <blumenthaleralex@gmail.com>
2021-01-05 12:02:34 -0500

    added edit/delete buttons for Recipe

--- src/components/recipes/Recipe.js
+++ src/components/recipes/Recipe.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CommentsContainer from '../containers/CommentsContainer.js'
 
 const Recipe = props => {
+        const currentUserId = props.currentUser.data.id
+        const recipeUserId = props.recipe.relationships.user.data.id
         const {attributes} = props.recipe
         return (
             <li>
@@ -15,6 +17,7 @@ const Recipe = props => {
                 <br />
                 {attributes.votes}
                 <br />
+                {currentUserId === recipeUserId ? <><button>Edit Recipe</button><br /><button>Delete Recipe</button></> : null}
                 <CommentsContainer recipe={props.recipe} currentUser={props.currentUser} comments={props.comments}/>
                 <br /><br />
             </li>
