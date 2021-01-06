a9ad32595e31d12861d8db715c13c17c1e3c3732
blumenthaler <blumenthaleralex@gmail.com>
2021-01-06 09:39:13 -0500

    pushes history when adding a new comment

--- src/components/comments/NewCommentButton.js
+++ src/components/comments/NewCommentButton.js
@@ -1,7 +1,22 @@
+import { useHistory, useRouteMatch } from 'react-router-dom'
+
 export const NewCommentButton = props => {
+    const recipeId = props.recipe.id
+    const cocktailId = props.recipe.relationships.cocktail.data.id
+    const history = useHistory()
+    const match = useRouteMatch()
+    const handleClick = () => {
+        props.triggerCommentForm()
+        if (match.url === '/profile/') {
+            history.push(`${match.url}cocktails/${cocktailId}/recipes/${recipeId}/comments/new`)
+        }
+        if (match.url === '/cocktails/') {
+            history.push(`${match.url}${cocktailId}/recipes/${recipeId}/comments/new`)
+        }
+    }
     return (
         <>
-        <button onClick={() => props.triggerCommentForm()}>Add Comment</button>
+        <button onClick={() => handleClick()}>Add Comment</button>
         <br /><br />
         </>
     ) 
