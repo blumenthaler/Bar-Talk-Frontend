73ab980d9972051f1517424288bfd1e0ec8ada81
blumenthaler <blumenthaleralex@gmail.com>
2021-01-06 11:25:41 -0500

    has access to history and match, passes as props

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import RecipesContainer from '../containers/RecipesContainer.js';
+import { useHistory, useRouteMatch } from 'react-router-dom'
 
 const User = props => {
+        const history = useHistory()
+        const match = useRouteMatch()
         let title;
         if (props.currentUser.data.id === props.user.id) {
             title = "Your Recipe:"
@@ -12,7 +15,7 @@ const User = props => {
         return (
             <>
                 <h4>{title}</h4> 
-                <RecipesContainer recipes={props.recipes} user={props.user} currentUser={props.currentUser} cocktail={props.cocktail} history={props.history} comments={props.comments} />
+                <RecipesContainer recipes={props.recipes} user={props.user} currentUser={props.currentUser} cocktail={props.cocktail} history={history} match={match} comments={props.comments} />
             </>
         )
 }
