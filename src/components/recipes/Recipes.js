1c8d5c805567160deb737a0afd1b0ec243460f75
blumenthaler <blumenthaleralex@gmail.com>
2021-01-06 11:26:51 -0500

    passes history, match as props to RecipeInput

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -3,14 +3,15 @@ import Recipe from './Recipe.js'
 import { useHistory, useRouteMatch } from 'react-router-dom'
 
 const Recipes = props => {
+    const history = useHistory()
+    const match = useRouteMatch()
     if (!props.recipes) {
         return (
             <h2>loading...</h2>
         )
     }
     else {
-        const history = useHistory()
-        const match = useRouteMatch()
+        
         return (            
             <ol>
                 {props.recipes.map(recipe => <Recipe key={recipe.id} history={history} match={match} recipe={recipe} userId={props.user.id}  currentUser={props.currentUser} user={props.user} comments={props.comments} editingRecipe={props.editingRecipe} />)}
