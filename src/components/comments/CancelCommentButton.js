db5cc8e63fc6b8c9491aea9ed404243208c0f9ba
blumenthaler <blumenthaleralex@gmail.com>
2021-01-06 09:43:25 -0500

    cancel comment button, pushes history back on click

---
+++ src/components/comments/CancelCommentButton.js
@@ -0,0 +1,13 @@
+import { useHistory, useRouteMatch } from 'react-router-dom'
+
+export const CancelCommentButton = props => {
+    const history = useHistory()
+    const match = useRouteMatch()
+    const handleClick = () => {
+        props.triggerCommentForm()
+        history.push(`${match.url}/`)
+    }
+    return (
+        <button onClick={() => handleClick()}>Cancel</button>
+    )
+}
