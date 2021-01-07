2d53c02df17323add1a45887e388cd431541d1f1
blumenthaler <blumenthaleralex@gmail.com>
2021-01-07 16:30:12 -0500

    RecipesContainer gets all recipes

--- src/components/containers/RecipesContainer.js
+++ src/components/containers/RecipesContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Recipes from '../recipes/Recipes.js';
 import RecipeInput from '../recipes/input/RecipeInput.js';
-import { addRecipe, editingRecipe, deleteRecipe } from '../../actions/recipes.js';
+import { addRecipe, editingRecipe, deleteRecipe, getAllRecipes } from '../../actions/recipes.js';
 import { NewRecipeButton } from '../recipes/input/NewRecipeButton.js';
 
 
@@ -23,14 +23,23 @@ class RecipesContainer extends React.Component {
         })
     }
 
+    componentDidMount() {
+        this.props.getAllRecipes()
+    }
+
     render() {
-        if (!this.props.cocktail) {
+        if ((!this.props.recipes.data) || (this.props.loading)) {
+            return (
+                <h1>Loading...</h1>
+            )
+        }
+        else if (!this.props.cocktail) {
             return (
                   <RecipeInput currentUser={this.props.currentUser} addRecipe={this.props.addRecipe} cocktail={this.props.cocktail} history={this.props.history} match={this.props.match} triggerRecipeForm={this.triggerRecipeForm} /> 
             )
         }
         else {
-            const filteredRecipes = this.props.recipes.filter(recipe => recipe.relationships.user.data.id === this.props.user.id)
+            const filteredRecipes = this.props.recipes.data.filter(recipe => recipe.relationships.user.data.id === this.props.user.id)
             return (
                 <div>
                     <Recipes user={this.props.user} recipes={filteredRecipes} currentUser={this.props.currentUser} editingRecipe={this.props.editingRecipe} deleteRecipe={this.props.deleteRecipe} />
@@ -45,9 +54,10 @@ class RecipesContainer extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        test: state.recipes
+        recipes: state.recipes.recipes,
+        loading: state.recipes.loading
     }
 }
 
 
-export default connect(mapStateToProps, {addRecipe, editingRecipe, deleteRecipe})(RecipesContainer)
+export default connect(mapStateToProps, {addRecipe, editingRecipe, deleteRecipe, getAllRecipes})(RecipesContainer)
