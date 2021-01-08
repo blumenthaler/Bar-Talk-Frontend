0decd63ffba8affc75846e282317e9c80a338d88
blumenthaler <blumenthaleralex@gmail.com>
2021-01-08 16:50:49 -0500

    Cocktail: updated props

--- src/components/cocktails/Cocktail.js
+++ src/components/cocktails/Cocktail.js
@@ -1,17 +1,26 @@
 import UsersContainer from '../containers/UsersContainer.js';
+import {useRouteMatch} from 'react-router-dom'
 
 export const Cocktail = props => {
-        const {attributes} = props.cocktail
-        if (props.cocktail.relationships.recipes.data.length === 0) {return null}
+        console.log(props)
+        const {cocktails, history, location, match, currentUser, profile} = props
+        console.log(props.match.params.cocktailId)
+        const cocktail = cocktails.cocktails.data.find(cocktail => cocktail.id === match.params.cocktailId)
+
+        if (cocktail.relationships.recipes.data.length === 0) {
+            return (
+                <h2>No Recipes for this Cocktail.</h2>
+            )
+        }
         else {
             return (
                 <li>
-                    {attributes.name} - {attributes.spirit.charAt(0).toUpperCase() + attributes.spirit.slice(1)}
+                    <h2>{cocktail.attributes.name} - {cocktail.attributes.spirit.charAt(0).toUpperCase() + cocktail.attributes.spirit.slice(1)}</h2>
                     
                     <UsersContainer 
-                        cocktail={props.cocktail} 
-                        currentUser={props.currentUser} 
-                        profile={props.profile} 
+                        cocktail={cocktail} 
+                        currentUser={currentUser} 
+                        profile={profile} 
                     />
                 </li>
             )
--- src/components/cocktails/Cocktails.js
+++ src/components/cocktails/Cocktails.js
@@ -11,22 +11,18 @@ export const Cocktails = props =>  {
         )
     }
     else {
-        console.log(props.cocktails.cocktails.data)
         let newMatch
-        console.log(match.path === '/profile/')
         if (match.path === '/profile/') {
             newMatch = match.path + 'cocktails/'
         }
         else {newMatch = match.path}
-        console.log(newMatch)
         return (
             <ol>
                 {props.cocktails.cocktails.data.map(cocktail => 
                     <>
                     <li>
                     <Link key={cocktail.id} to={`${newMatch}${cocktail.id}`}>
-                        {`${cocktail.attributes.name} - ${cocktail.attributes.spirit}`}
-                        
+                        {`${cocktail.attributes.name} - ${cocktail.attributes.spirit}`}    
                     </Link>
                     </li>
                     </>
