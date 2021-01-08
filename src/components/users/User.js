bc9b42aa4b069b609ca3c401eba29b046bb20e0d
blumenthaler <blumenthaleralex@gmail.com>
2021-01-08 13:19:18 -0500

    added pluralize package; pluralizes title in User component if user has more than one recipe for a given cocktail

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import RecipesContainer from '../containers/RecipesContainer.js';
 import { useHistory, useRouteMatch } from 'react-router-dom'
+import pluralize from 'pluralize'
 
 const User = props => {
         const history = useHistory()
@@ -12,25 +13,31 @@ const User = props => {
         else {
             title =  `${props.user.attributes.username}'s Recipe:`
         }
+
+        // pluralize title based on number of user's recipes
+        let recipes = props.recipes.filter(recipe => recipe.relationships.user.data.id === props.user.id)
+        let split = title.split(":")[0].split(" ")
+        const plural = pluralize(split[split.length - 1], recipes.length)
+        split[split.length -1] = plural
+        console.log(split)
+        title = split.join(" ")
+        title += ':'
+
+        
         return (
             <>
-                {/* pluralize title based on number of recipes */}
                 <h4>{title}</h4> 
                 
                 <RecipesContainer 
-                
                 // these make sense to me to keep being passed
                 // this way, I can make comparisons/filter the included data when I getAllRecipes in the container 
-                user={props.user} 
-                currentUser={props.currentUser} 
-                cocktail={props.cocktail} 
+                    user={props.user} 
+                    currentUser={props.currentUser} 
+                    cocktail={props.cocktail} 
 
                 // do I need to incoporate these here?
                 // investigate
                 history={history} match={match} 
-
-                // get recipes from getAllRecipes in the container; not passed from above
-                // recipes={props.recipes} 
                  />
             </>
         )
