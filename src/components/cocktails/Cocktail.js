3800b5069de1499ac327f8e2965a2ca048b8492d
blumenthaler <blumenthaleralex@gmail.com>
2021-01-08 13:57:20 -0500

    changed Cocktail to functional component (stateless); removed unused code

--- src/components/cocktails/Cocktail.js
+++ src/components/cocktails/Cocktail.js
@@ -1,28 +1,19 @@
-import React from 'react'
 import UsersContainer from '../containers/UsersContainer.js';
 
-class Cocktail extends React.Component {
-
-    render() {
-        const {attributes} = this.props.cocktail
-        
-        if (this.props.cocktail.relationships.recipes.data.length === 0) {return null}
-
+export const Cocktail = props => {
+        const {attributes} = props.cocktail
+        if (props.cocktail.relationships.recipes.data.length === 0) {return null}
         else {
             return (
                 <li>
                     {attributes.name} - {attributes.spirit.charAt(0).toUpperCase() + attributes.spirit.slice(1)}
                     
-                    <UsersContainer cocktail={this.props.cocktail} currentUser={this.props.currentUser} profile={this.props.profile} 
-                    
-                    // do I need this here?
-                    history={this.props.history} 
-                    
+                    <UsersContainer 
+                        cocktail={props.cocktail} 
+                        currentUser={props.currentUser} 
+                        profile={props.profile} 
                     />
                 </li>
             )
         }
     }
-}
-
-export default Cocktail
