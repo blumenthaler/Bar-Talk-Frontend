1433188d4743deeeaec093a4dd8af0baa2f63bc5
blumenthaler <blumenthaleralex@gmail.com>
2021-01-08 16:49:20 -0500

    Cocktails: created links for Cocktail components

--- src/components/cocktails/Cocktails.js
+++ src/components/cocktails/Cocktails.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { Cocktail } from './Cocktail.js'
+import {Link, useRouteMatch} from 'react-router-dom'
+
 
 export const Cocktails = props =>  {
+    const match = useRouteMatch()
     if ((props.loading) || (!props.cocktails)) {
         return (
             <h2>loading...</h2>
         )
     }
     else {
+        console.log(props.cocktails.cocktails.data)
+        let newMatch
+        console.log(match.path === '/profile/')
+        if (match.path === '/profile/') {
+            newMatch = match.path + 'cocktails/'
+        }
+        else {newMatch = match.path}
+        console.log(newMatch)
         return (
             <ol>
-                {props.cocktails.map(cocktail => <Cocktail 
-                        key={cocktail.id} 
-                        cocktail={cocktail}  
-                        currentUser={props.currentUser} 
-                        profile={props.profile} 
-                    />)}
+                {props.cocktails.cocktails.data.map(cocktail => 
+                    <>
+                    <li>
+                    <Link key={cocktail.id} to={`${newMatch}${cocktail.id}`}>
+                        {`${cocktail.attributes.name} - ${cocktail.attributes.spirit}`}
+                        
+                    </Link>
+                    </li>
+                    </>
+                )}
             </ol>
         )
     }
