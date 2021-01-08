8e54c70b287c3534054400e854fbf56f4c43bbb3
blumenthaler <blumenthaleralex@gmail.com>
2021-01-08 11:24:10 -0500

    handles adding, deleting recipe (state.users.included)

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -14,6 +14,43 @@ export default (state = {
                 users: action.users,
                 loading: false
             }
+        case "ADD_NEW_RECIPE":
+            const newRecipeUser = action.recipe.included.find(data => data.type === 'user')
+            if (!state.users.data.find(user => user.id === newRecipeUser.id)) {
+                const usersWithNew = [
+                    ...state.users.data,
+                    newRecipeUser
+                ]
+                return {
+                    ...state,
+                    users: {
+                        ...state.users,
+                        data: usersWithNew,
+                        included: [...state.users.included, action.recipe.data]
+                    },
+                    loading: false
+                }
+            }
+            else {
+                return {
+                    ...state,
+                    users: {
+                        ...state.users,
+                        included: [...state.users.included, action.recipe.data]
+                    },
+                    loading: false
+                }
+            }
+        case "DELETE_RECIPE":
+            const deletedRecipe = state.users.included.filter(recipe => recipe.id === action.recipe.id ? false : true)
+            return {
+                ...state,
+                users: {
+                    ...state.users,
+                    included: deletedRecipe
+                },
+                loading: false
+            }
         default:
             return state;
     }
