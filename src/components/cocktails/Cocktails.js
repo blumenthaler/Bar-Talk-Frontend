635dda9bf20daf593844cbf03669dd6cbe374291
blumenthaler <blumenthaleralex@gmail.com>
2021-01-08 13:59:46 -0500

    changed Cocktails to stateless functional component

--- src/components/cocktails/Cocktails.js
+++ src/components/cocktails/Cocktails.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import Cocktail from './Cocktail.js'
+import { Cocktail } from './Cocktail.js'
 
-class Cocktails extends React.Component {
-
-    render() {
-        if ((this.props.loading) || (!this.props.cocktails)) {
-            return (
-                <h2>loading...</h2>
-            )
-        }
-        else {
-            return (
-                <ol>
-                    {this.props.cocktails.map(cocktail => <Cocktail key={cocktail.id} cocktail={cocktail}  currentUser={this.props.currentUser} profile={this.props.profile} 
-                    
-                    // do I need this here?
-                    history={this.props.history} 
-                    
-                     />)}
-                </ol>
-            )
-        }
+export const Cocktails = props =>  {
+    if ((props.loading) || (!props.cocktails)) {
+        return (
+            <h2>loading...</h2>
+        )
+    }
+    else {
+        return (
+            <ol>
+                {props.cocktails.map(cocktail => <Cocktail 
+                        key={cocktail.id} 
+                        cocktail={cocktail}  
+                        currentUser={props.currentUser} 
+                        profile={props.profile} 
+                    />)}
+            </ol>
+        )
     }
 }
-
-export default Cocktails
