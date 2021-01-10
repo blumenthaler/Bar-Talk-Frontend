9847795d127ad9b5c3b0c21713d95a35a46f67db
blumenthaler <blumenthaleralex@gmail.com>
2021-01-10 18:46:15 -0500

    NewRecipeButton: correctly handles routes for new recipe

--- src/components/recipes/input/NewRecipeButton.js
+++ src/components/recipes/input/NewRecipeButton.js
@@ -3,21 +3,18 @@ import { useHistory, useRouteMatch } from 'react-router-dom'
 export const NewRecipeButton = props => {
     const history = useHistory()
     const match = useRouteMatch()
+    let matchUrl
+    match.url.charAt(match.url.length-1) === '/' ? matchUrl = match.url : matchUrl = `${match.url}/`
+
     const handleClick = () => {
         props.triggerRecipeForm()
-        if (props.cocktail) {
-            if (match.url === '/cocktails/') {
-                history.push(`${match.url}${props.cocktail.id}/recipes/new`)
-            }
-            else {
-                history.push(`${match.url}cocktails/${props.cocktail.id}/recipes/new`)
-            }
+        if (matchUrl.includes('profile')) {
+            history.push(`${matchUrl}recipes/new`)
         }
-        else {
-            history.push(`${match.url}recipes/new`)
+        else if (matchUrl.includes('cocktails')) {
+                history.push(`${matchUrl}recipes/new`)
         }
     }
-    
     return (
         <>
         <button onClick={() => handleClick()}>Add a New Recipe to this Cocktail</button>
