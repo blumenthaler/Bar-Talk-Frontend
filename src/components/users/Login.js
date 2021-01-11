adbb8d95e7679f04f347c76b21391ab176903b0c
blumenthaler <blumenthaleralex@gmail.com>
2021-01-11 16:12:12 -0500

    Login: styled with Material UI

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateLoginForm } from '../../actions/loginForm.js';
 import { login } from '../../actions/currentUser.js';
-
-
+import { Button } from '@material-ui/core';
+import {textFieldUseStyles} from '../../material-ui/textFieldUseStyles.js'
+import TextField from '@material-ui/core/TextField';
 
 const Login = props => {
-
     const {loginForm, updateLoginForm, login, history} = props
+    const classes = textFieldUseStyles()
 
     const handleChange = event => {
         const { name, value } = event.target
@@ -18,17 +19,17 @@ const Login = props => {
         updateLoginForm(updatedFormInfo)
     }
 
-    const handleSubmit = event => {
-        event.preventDefault()
+    const handleSubmit = () => {
         login(loginForm)
         history.push('/profile/')
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder="Username" type="text" name="username" onChange={handleChange} value={loginForm.username}></input>
-            <input placeholder="Password" type="password" name="password" onChange={handleChange} value={loginForm.password}></input>
-            <input type="submit" value="Log In"></input>
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField id="login-username" label="Username" name="username" value={loginForm.username} onChange={handleChange} />
+            <TextField id="login-password" label="Password" name="password" type="password" value={loginForm.password} onChange={handleChange} />
+            <Button variant="contained" color="primary" onClick={() => handleSubmit()}>Log In</Button>
+            {/* <input type="submit" visible="hidden"></input> */}
         </form>
     )
 }
@@ -38,7 +39,5 @@ const mapStateToProps = state => {
         loginForm: state.loginForm
     }
 }
-
-
 
 export default connect(mapStateToProps, { updateLoginForm, login })(Login)
