b6fbffc93a6e8fd3ad3757b5ec310e5cf2c8c2c0
blumenthaler <blumenthaleralex@gmail.com>
2021-01-11 11:44:38 -0500

    Cocktail: shows message when last recipe is deleted

--- src/components/cocktails/Cocktail.js
+++ src/components/cocktails/Cocktail.js
@@ -1,10 +1,22 @@
 import UsersContainer from '../containers/UsersContainer.js';
-import {useRouteMatch} from 'react-router-dom'
+import {useRouteMatch, Link} from 'react-router-dom'
 
 export const Cocktail = props => {
         const {cocktails, history, location, match, currentUser, profile} = props
-        const cocktail = cocktails.cocktails.data.find(cocktail => cocktail.id === match.params.cocktailId)
+        const cocktail = cocktails.data.find(cocktail => cocktail.id === match.params.cocktailId)
 
+        const handleClick = () => {
+            history.push('/recipes/new')
+        }
+
+        if (!cocktail) {
+            return (
+            <>
+            <h2>This cocktail no longer exists.</h2>
+            <button onClick={() => handleClick()}>Click here to add a new recipe</button>
+            </>
+            )
+        }
         if (cocktail.relationships.recipes.data.length === 0) {
             return (
                 <h2>No Recipes for this Cocktail.</h2>
