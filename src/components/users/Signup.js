d3d316d0f2d24fb852cd01f0d4f5635b169ee99b
blumenthaler <blumenthaleralex@gmail.com>
2021-01-11 18:30:35 -0500

    Signup: implemented styling with Material UI

--- src/components/users/Signup.js
+++ src/components/users/Signup.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateSignupForm } from '../../actions/signupForm.js';
 import { signup } from '../../actions/currentUser.js';
-
+import { Button } from '@material-ui/core';
+import {textFieldUseStyles} from '../../material-ui/textFieldUseStyles.js'
+import TextField from '@material-ui/core/TextField';
 
 
 const Signup = ({signupForm, updateSignupForm, signup, history}) => {
+
+    const classes = textFieldUseStyles()
 
     const handleChange = event => {
         const { name, value } = event.target
@@ -16,16 +20,18 @@ const Signup = ({signupForm, updateSignupForm, signup, history}) => {
         updateSignupForm(updatedFormInfo)
     }
 
-    const handleSubmit = event => {
-        event.preventDefault()
+    const handleSubmit = () => {
         signup(signupForm)
         history.push('/profile/')
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder="Username" type="text" name="username" onChange={handleChange} value={signupForm.username}></input>
-            <input placeholder="Password" type="password" name="password" onChange={handleChange} value={signupForm.password}></input>
-            <input type="submit" value="Sign Up"></input>
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField id="submit-username" label="Username" name="username" value={signupForm.username} onChange={handleChange} />
+
+            <TextField id="signup-password" label="Password" name="password" type="password" value={signupForm.password} onChange={handleChange} />
+
+            <Button variant="contained" color="primary" onClick={() => handleSubmit()}>Sign Up</Button>
+            {/* <input type="submit" value="Sign Up"></input> */}
         </form>
     )
 }
