e7b4107317d0639502be6574b06003b8d9317f9a
blumenthaler <blumenthaleralex@gmail.com>
2021-01-11 15:43:44 -0500

    Created unique useStyles functions for NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import {logout} from '../actions/currentUser.js'
-import {useStyles} from '../material-ui/useStyles.js'
+import {useStyles} from '../material-ui/navBarUseStyles.js'
 
 
 const NavBar = props => {
