a437437848cb247768f5a3bde8c96af8a8d5628a
blumenthaler <blumenthaleralex@gmail.com>
2021-01-12 14:49:28 -0500

    textFieldUseStyles: override default m-ui style

--- src/material-ui/textFieldUseStyles.js
+++ src/material-ui/textFieldUseStyles.js
@@ -1,10 +1,37 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const textFieldUseStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '20ch',
+      color: '#e4be9e',
+    },
+    '& .MuiInputBase-input': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiInput-input': {
+      color: '#e4be9e',
+    },
+    '& .MuiInput-underline:before': {
+      borderBottomColor: '#e4be9e',
+    },
+    '& label.Mui-focused': {
+      color: '#f2f6d0',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#f2f6d0',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#f2f6d0',
+      },
+      '&:hover fieldset': {
+        borderColor: '#f2f6d0',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#f2f6d0',
       },
     },
+  },
   }));
