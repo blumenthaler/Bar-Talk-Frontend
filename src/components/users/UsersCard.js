6e8cfd57e9ec12c8a0e02950fbc0b536735e94a1
blumenthaler <blumenthaleralex@gmail.com>
2021-01-13 14:15:36 -0500

    UsersCard: created UsersCard

---
+++ src/components/users/UsersCard.js
@@ -0,0 +1,23 @@
+import {Users} from './Users'
+import UsersContainer from '../containers/UsersContainer.js'
+import Card from '@material-ui/core/Card';
+import {cardUseStyles} from '../../material-ui/cardUseStyles.js'
+
+
+export const UsersCard = props => {
+    const {cocktail, currentUser, profile} = props
+    const classes = cardUseStyles()
+    
+    return (
+        <>
+        <div style={{ display:'flex', justifyContent:'center' }}>
+            <Card className={classes.root} style={{'min-width': '600px'}}>
+                <UsersContainer 
+                    cocktail={cocktail} 
+                    currentUser={currentUser} 
+                    profile={profile} />
+            </Card>
+        </div>
+        </>
+    )
+}
