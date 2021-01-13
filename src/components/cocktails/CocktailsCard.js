fc01925e512d3d898305f4a3dad338e65f9d2ddf
blumenthaler <blumenthaleralex@gmail.com>
2021-01-13 14:08:25 -0500

    CocktailsCard: centered, resized CocktailsCard

--- src/components/cocktails/CocktailsCard.js
+++ src/components/cocktails/CocktailsCard.js
@@ -2,12 +2,18 @@ import {Cocktails} from './Cocktails'
 import Card from '@material-ui/core/Card';
 import {cardUseStyles} from '../../material-ui/cardUseStyles.js'
 
+
 export const CocktailsCard = props => {
     const classes = cardUseStyles()
+    
     return (
-        <Card className={classes.root}>
-            Cocktails:
-            <Cocktails cocktails={props.cocktails}/>
-        </Card>
+        <>
+        <div style={{ display:'flex', justifyContent:'center' }}>
+            <Card className={classes.root} style={{'min-width': '300px'}}>
+                Cocktails:
+                <Cocktails cocktails={props.cocktails}/>
+            </Card>
+        </div>
+        </>
     )
 }
