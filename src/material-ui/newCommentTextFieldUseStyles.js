1af896b75d43b87aecf401c16d3b813b8c9383b5
blumenthaler <blumenthaleralex@gmail.com>
2021-01-14 16:24:17 -0500

    created styles for new comment text fields

---
+++ src/material-ui/newCommentTextFieldUseStyles.js
@@ -0,0 +1,38 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const newCommentTextFieldUseStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '20ch',
+      color: '#f2f6d0',
+    },
+    '& .MuiInputBase-input': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiInput-input': {
+      color: '#f2f6d0',
+      'font-weight': 'bold',
+    },
+    '& .MuiInput-underline:before': {
+      borderBottomColor: '#e4be9e',
+    },
+    '& label.Mui-focused': {
+      color: '#f2f6d0',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#f2f6d0',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#f2f6d0',
+      },
+      '&:hover fieldset': {
+        borderColor: '#f2f6d0',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#f2f6d0',
+      },
+    },
+  },
+  }));
