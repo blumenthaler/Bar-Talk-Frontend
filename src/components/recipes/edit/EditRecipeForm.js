f10e58804fc3aa4c9350aed0d704e692f81046e2
blumenthaler <blumenthaleralex@gmail.com>
2021-01-14 17:37:15 -0500

    EditRecipeForm: updated button layout; wrapped in Card

--- src/components/recipes/edit/EditRecipeForm.js
+++ src/components/recipes/edit/EditRecipeForm.js
@@ -18,8 +18,8 @@ export default class EditRecipeForm extends React.Component {
         }
     }
 
-    handleOnSubmit = () => {
-        // event.preventDefault()
+    handleOnSubmit = (event) => {
+        if (event) {event.preventDefault()}
         this.props.editingRecipe(this.props.recipe.id, this.state)
         this.props.triggerEditingForm()
         this.props.history.push(`${this.props.match.url}/`)
@@ -34,23 +34,20 @@ export default class EditRecipeForm extends React.Component {
     render() {
         const { spirit } = this.props.recipe.attributes
         return (
-            <form onSubmit={this.handleOnSubmit}>
+            <form className={this.props.classes.root} onSubmit={event => this.handleOnSubmit(event)}>
                 <div id={this.props.recipe.id + '_name_edit'}>Name: {this.props.recipe.attributes.name}</div>
                 <div id={this.props.recipe.id + '_spirit_edit'}>Spirit: {spirit.charAt(0).toUpperCase() + spirit.slice(1)}</div><br />
 
 
                 <Input placeholder={`Ingredients: ${this.state.ingredients}`} value={this.state.ingredients} name="ingredients" onChange={this.handleOnChange} /><br />
 
-                {/* <label>Ingredients: </label><textarea type="text" onChange={this.handleOnChange} name="ingredients" value={this.state.ingredients} /><br /> */}
+                <Input placeholder={`Garnish: ${this.state.garnish}`} value={this.state.garnish} name="garnish" onChange={this.handleOnChange} /><br />
 
-                <label>Garnish: </label><input type="text" onChange={this.handleOnChange} name="garnish" value={this.state.garnish}></input><br />
-
-                <label>Notes: </label><textarea onChange={this.handleOnChange} name="notes" value={this.state.notes} /><br /><br />
-
+                <Input placeholder={`Notes: ${this.state.notes}`} value={this.state.notes} name="notes" onChange={this.handleOnChange} /><br />
 
                 <Button variant="contained" id='edit-recipe-button' onClick={this.handleOnSubmit}>Edit</Button>
-                {/* <input type="submit" value="Edit"></input><br /> */}
-                <CancelEditButton triggerEditingForm={this.props.triggerEditingForm}/><br /><br />
+                <CancelEditButton triggerEditingForm={this.props.triggerEditingForm}/>
+                <input style={{marginBottom: "-100px"}} type="submit" className="hidden" value="Edit" />
             </form>
         )
     }
