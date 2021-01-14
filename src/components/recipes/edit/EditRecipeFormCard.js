eca73a055d5c6ceff2b11bf526b9a0b9b370c975
blumenthaler <blumenthaleralex@gmail.com>
2021-01-14 17:35:04 -0500

    created card for EditRecipeForm

---
+++ src/components/recipes/edit/EditRecipeFormCard.js
@@ -0,0 +1,26 @@
+import EditRecipeForm from "./EditRecipeForm.js"
+import Card from '@material-ui/core/Card';
+import {editRecipeUseStyles} from '../../../material-ui/editRecipeUseStyles.js'
+import {textFieldUseStyles} from '../../../material-ui/textFieldUseStyles.js'
+
+
+export const EditRecipeFormCard = props => {
+    const classes = editRecipeUseStyles()
+    const formClasses = textFieldUseStyles()
+    return (
+        <>
+        <div style={{ display:'flex', justifyContent:'center' }}>
+            <Card className={classes.root} style={{minWidth: '450px', marginLeft: '-10%'}}>
+                <EditRecipeForm 
+                    classes={formClasses}
+                    editingRecipe={props.editingRecipe} 
+                    recipe={props.recipe} 
+                    triggerEditingForm={props.triggerEditingForm} 
+                    history={props.history}
+                    match={props.match}
+                />
+            </Card>
+        </div>
+        </>
+    )
+}
