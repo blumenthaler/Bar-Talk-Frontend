97c47343241f751e96c9471b0f2d095c3c98dd27
blumenthaler <blumenthaleralex@gmail.com>
2021-01-13 19:28:35 -0500

    cardUseStyles: updated background color

--- src/material-ui/cardUseStyles.js
+++ src/material-ui/cardUseStyles.js
@@ -4,7 +4,7 @@ export const cardUseStyles = makeStyles((theme) => ({
     root: {
       ...theme.typography.button,
       color: "#71697A",
-      backgroundColor: '#d0e1d4',
+      backgroundColor: '#e4be9e',
       padding: theme.spacing(1),
       'max-width': '300px',
       'font-weight': 'bold',
