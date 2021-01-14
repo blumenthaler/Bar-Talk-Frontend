f41e17887edcf456954deef68f33ecb6e36bc394
blumenthaler <blumenthaleralex@gmail.com>
2021-01-13 19:34:19 -0500

    RecipeInputCard: created conditional for recipe form styles

--- src/components/recipes/input/RecipeInputCard.js
+++ src/components/recipes/input/RecipeInputCard.js
@@ -1,17 +1,21 @@
 import Card from '@material-ui/core/Card';
 import {cardUseStyles} from '../../../material-ui/cardUseStyles.js'
+import {textFieldUseStyles} from '../../../material-ui/textFieldUseStyles.js'
+import {newRecipeTextFieldUseStyles} from '../../../material-ui/newRecipeTextFieldUseStyles.js'
 import RecipeInput from './RecipeInput.js'
 
 
 export const RecipeInputCard = props => {
     const classes = cardUseStyles()
-
+    let formClasses
     const findStyle = () => {
         if (props.cocktail) {
-            return {'min-width': '550px', backgroundColor: '#e4be9e', 'margin-right': '30px', 'margin-left': 'auto'} 
+            formClasses = textFieldUseStyles()
+            return {minWidth: '550px', backgroundColor: '#71697a', marginRight: '30px', marginLeft: 'auto'} 
         }
         else {
-            return {'min-width': '550px', backgroundColor: '#d0e1d4', 'margin-right': '20%', 'margin-left': '20%'}
+            formClasses = newRecipeTextFieldUseStyles()
+            return {minWidth: '550px', backgroundColor: '#d0e1d4', marginRight: '20%', marginLeft: '20%', color: '#71697a'}
         }
     }
     return (
@@ -24,7 +28,8 @@ export const RecipeInputCard = props => {
                     cocktail={props.cocktail} 
                     history={props.history} 
                     match={props.match} 
-                    triggerRecipeForm={props.triggerRecipeForm} 
+                    triggerRecipeForm={props.triggerRecipeForm}
+                    classes={formClasses} 
                 /> 
             </Card>
         </div>
