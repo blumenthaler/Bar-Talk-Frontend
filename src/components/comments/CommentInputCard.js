84b2a371977b6fa6ce47ae7ad5490e392738fdc3
blumenthaler <blumenthaleralex@gmail.com>
2021-01-14 16:23:50 -0500

    created CommentInputCard

---
+++ src/components/comments/CommentInputCard.js
@@ -0,0 +1,26 @@
+import CommentInput from './CommentInput.js';
+import Card from '@material-ui/core/Card';
+import {cardUseStyles} from '../../material-ui/cardUseStyles.js'
+import {newCommentTextFieldUseStyles} from '../../material-ui/newCommentTextFieldUseStyles.js'
+
+export const CommentInputCard = props => {
+    const classes = cardUseStyles()
+    const formClasses = newCommentTextFieldUseStyles()
+    
+    return (
+        <>
+        <div style={{ display:'flex', justifyContent:'center' }}>
+            <Card className={classes.root} style={{maxWidth: '1000px', minWidth: '300px', backgroundColor: '#45062e'}}>
+                <CommentInput
+                    classes={formClasses}
+                    user={props.user} 
+                    recipe={props.recipe} 
+                    triggerCommentForm={props.triggerCommentForm} 
+                    addComment={props.addComment} 
+                    history={props.history} 
+                    match={props.match}  />
+            </Card>
+        </div>
+        </>
+    )
+}
