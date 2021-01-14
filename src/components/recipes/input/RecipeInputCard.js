18cd79cdcccd12cce85bacf56b88e54d6d48e6df
blumenthaler <blumenthaleralex@gmail.com>
2021-01-14 10:32:32 -0500

    RecipeInputCard: updated textfieldstyles

--- src/components/recipes/input/RecipeInputCard.js
+++ src/components/recipes/input/RecipeInputCard.js
@@ -7,20 +7,19 @@ import RecipeInput from './RecipeInput.js'
 
 export const RecipeInputCard = props => {
     const classes = cardUseStyles()
-    let formClasses
+    let formClasses = textFieldUseStyles()
     const findStyle = () => {
         if (props.cocktail) {
-            formClasses = textFieldUseStyles()
-            return {minWidth: '550px', backgroundColor: '#71697a', marginRight: '30px', marginLeft: 'auto'} 
+            return {minWidth: '550px', backgroundColor: '#45062e', marginRight: '30px', marginLeft: '-20px'} 
         }
         else {
-            formClasses = newRecipeTextFieldUseStyles()
-            return {minWidth: '550px', backgroundColor: '#d0e1d4', marginRight: '20%', marginLeft: '20%', color: '#71697a'}
+            return {minWidth: '550px', backgroundColor: '#45062e', marginRight: '20%', marginLeft: '20%', color: '#71697a', border: 'none', boxShadow: "none"}
         }
     }
     return (
         <>
-        <div style={{ display:'flex', justifyContent:'center' }}>
+        <br />
+        <div>
             <Card className={classes.root} style={findStyle()}>
             <RecipeInput 
                     currentUser={props.currentUser} 
