c55cf3eabe778b746455a9dfea5fad3e7dcd8c5f
blumenthaler <blumenthaleralex@gmail.com>
2021-01-15 13:24:32 -0500

    created useStyles function for recipe input won cocktails index page(removed inline css)

---
+++ src/material-ui/recipeInputWithCocktail.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const recipeInputWithCocktailUseStyles = makeStyles((theme) => ({
+    root: {
+      ...theme.typography.button,
+      color: "#7f055f",
+      backgroundColor: '#45062e',
+      padding: theme.spacing(1),
+      maxWidth: '300px',
+      minWidth: '550px',
+      fontWeight: 'bold',
+      marginRight: '30px', 
+      marginLeft: '-5%'
+    },
+  }));
