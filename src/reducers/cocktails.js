08fb972b65fee068b683bf69e42c7eef919f4260
blumenthaler <blumenthaleralex@gmail.com>
2021-01-15 15:58:49 -0500

    Cocktails reducer: fixed bug, was adding same recipe twice

--- src/reducers/cocktails.js
+++ src/reducers/cocktails.js
@@ -16,30 +16,18 @@ export default (state = {
             }
         case "ADD_NEW_RECIPE":
             const newRecipeCocktail = action.recipe.included.find(data => data.type === 'cocktail')
-            const cocktailsWithNew = [
-                ...state.cocktails.data,
-                action.recipe.included.find(data => data.type === 'cocktail')
-            ]
+
             if (!state.cocktails.data.find(cocktail => cocktail.id === newRecipeCocktail.id)) {
-                return {
-                    ...state,
-                    cocktails: {
-                        ...state.cocktails,
-                        data: cocktailsWithNew,
-                        included: [...state.cocktails.included, action.recipe.data]
-                    },
-                    loading: false
-                }
+                Object.assign(state.cocktails.data[state.cocktails.data.findIndex(cocktail => cocktail.id === newRecipeCocktail.id)], newRecipeCocktail)
             }
-            else {
-                return {
-                    ...state,
-                    cocktails: {
-                        ...state.cocktails,
-                        included: [...state.cocktails.included, action.recipe.data]
-                    },
-                    loading: false
-                }
+
+            return {
+                ...state,
+                cocktails: {
+                    ...state.cocktails,
+                    included: [...state.cocktails.included, action.recipe.data]
+                },
+                loading: false
             }
         case "DELETE_RECIPE":
             const deletedRecipe = state.cocktails.included.filter(data => ((data.type === 'recipe') && (data.id === action.recipe.id)) ? false : true)
